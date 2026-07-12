301c7b54868838a5c7492459546c019add80f5f9
Parshva Mehta <23010101166@darshan.ac.in>
2026-07-12 12:06:17 +0530

    Initialize controller for business logics

---
+++ Backend/Controller/AllocationsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AllocationsController : ControllerBase
+    {
+    }
+}
